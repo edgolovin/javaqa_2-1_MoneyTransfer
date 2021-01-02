a28f1e768b48f6cc3bbb4e56c13c67636d7f46d1
Evgeniy Golovin <edgolovin@yandex.ru>
2021-01-02 17:51:09 +0700

    add source code

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		int current_user_deposit = 2_000_000_000;
+		int user_transfer = 500_000_000;
+		int final_user_deposit = current_user_deposit + user_transfer;
+		System.out.println(final_user_deposit);
+	}
+}
